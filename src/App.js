7b4e906c6436d85ee1d45189aefc3248c422bbd4
Kumar Anuj <kumar.anuj@magicsw.com>
2022-02-12 20:52:34 +0530

    Added New User list and  react-router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css'
 import HomePage from './components/pages/HomePage/HomePage';
 import Sidebar from './components/Sidebar/Sidebar';
 import Topbar from './components/TopBar/Topbar'
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import UserList from './components/pages/userlist/UserList';
 function App() {
   return (
-    <div className='App'>
+    <BrowserRouter className='App'>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <HomePage />
+        <Routes>
+          <Route exact path='/' element={<HomePage/>} />
+          <Route path='/users' element={<UserList/>}/>
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
